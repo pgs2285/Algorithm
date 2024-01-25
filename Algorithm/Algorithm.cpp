50bec61269277ba38778509305a8db7efe2e58c3
pgs2285 <wldnjs2284@naver.com>
2024-01-25 14:01:28 +0900

    Algorithm basic END 이 이후는 백준 풀이 진행 repository따로 제작

--- Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm.cpp
@@ -2,22 +2,22 @@
 #include <Windows.h>
 #include <vector>
 using namespace std;
-int cache[100][100] = { 0, };
+//int cache[100][100] = { 0, };
 
 int combination(int n, int k)
 {
 	if (k == 0 || n == k)
 		return 1;
-	int& ret = cache[n][k];
-	if (ret != -1)
-		return ret;
+	//int& ret = cache[n][k];
+	//if (ret != -1)
+		//return ret;
 
-	return ret = combination(n - 1, k - 1) + combination(n - 1, k);
+	return combination(n - 1, k - 1) + combination(n - 1, k);
 }
 
 int main()
 {
-	::memset(cache, -1, sizeof(cache));
+	//::memset(cache, -1, sizeof(cache));
 	__int64 start = GetTickCount64();
 	int lotto = combination(45, 6);
 	__int64 end = GetTickCount64();
